92589af6dd95147c8f0272aed36171f7a189d6ab
i.kłusek <J6ZkqwVUn>
2022-07-13 23:25:14 +0200

    AppStrucure and DataFlow Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import AddPostForm from './features/posts/AddPostForm';
+import PostsList from './features/posts/PostsList';
 
 function App() {
-  return (
-    <main className="App">
-      <h1> Hi</h1>
-    </main>
-  );
+    return (
+        <main className="App">
+            <AddPostForm />
+            <PostsList />
+        </main>
+    );
 }
 
 export default App;
