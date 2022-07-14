b5594dbf58d0c0f500bb71a53d82414252d1ace3
i.kłusek <J6ZkqwVUn>
2022-07-14 23:32:45 +0200

    router and extraReducers(Builder) functionality

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -9,15 +9,39 @@ const initialState = {
     status: 'idle',
     error: null,
 };
-
+// logic is in extraReducers(builder) {
+// builder
+// .addCase(fetchPosts
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     const response = await axios.get(POSTS_URL);
     return response.data;
 });
-
+// return from createAsyncThunk goes to extraReducers with the function name sa a payload
 export const addNewPost = createAsyncThunk('posts/addNewPosts', async (initialPost) => {
     const response = await axios.post(POSTS_URL, initialPost);
     return response.data;
+});
+
+export const updatePost = createAsyncThunk('posts/updatePost', async (initialPost) => {
+    const { id } = initialPost;
+    try {
+        const response = await axios.put(`${POSTS_URL}/${id}`, initialPost);
+        return response.data;
+    } catch (err) {
+        //return err.message;
+        return initialPost; // only for testing Redux!
+    }
+});
+
+export const deletePost = createAsyncThunk('posts/deletePost', async (initialPost) => {
+    const { id } = initialPost;
+    try {
+        const response = await axios.delete(`${POSTS_URL}/${id}`);
+        if (response?.status === 200) return initialPost;
+        return `${response?.status}: ${response?.statusText}`;
+    } catch (err) {
+        return err.message;
+    }
 });
 
 const postsSlice = createSlice({
@@ -95,6 +119,27 @@ const postsSlice = createSlice({
                 };
                 console.log(action.payload);
                 state.posts.push(action.payload);
+            })
+            .addCase(updatePost.fulfilled, (state, action) => {
+                if (!action.payload?.id) {
+                    console.log('Update could not complete');
+                    console.log(action.payload);
+                    return;
+                }
+                const { id } = action.payload;
+                action.payload.date = new Date().toISOString();
+                const posts = state.posts.filter((post) => post.id !== id);
+                state.posts = [...posts, action.payload];
+            })
+            .addCase(deletePost.fulfilled, (state, action) => {
+                if (!action.payload?.id) {
+                    console.log('Delete could not complete');
+                    console.log(action.payload);
+                    return;
+                }
+                const { id } = action.payload;
+                const posts = state.posts.filter((post) => post.id !== id);
+                state.posts = posts;
             });
     },
 });
@@ -102,6 +147,8 @@ const postsSlice = createSlice({
 export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
+
+export const selectPostById = (state, postId) => state.posts.posts.find((post) => post.id === postId);
 
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import { store } from './app/store';
 import { Provider } from 'react-redux';
 import { fetchUsers } from './features/users/usersSlice';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>
+    // <React.StrictMode>
+    <Provider store={store}>
+        <Router>
+            <Routes>
+                <Route path="/*" element={<App />} /> {/*  '/*' - allows for nested routes */}
+            </Routes>
+        </Router>
+    </Provider>
+    // </React.StrictMode>
 );
